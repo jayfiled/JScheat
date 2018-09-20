0d656560fab3b8e01eb3b5aa8b20794142850d48
Jayfiled <jayfiled@gmail.com>
2018-09-20 21:56:49 +1000

    style the accordion and add basic JS notes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 let accordion = document.getElementsByClassName('accordion');
 
-for (let i = 0; i <= accordion.length; i++) {
+for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
-        this.classList.Toggle('active');
+        this.classList.toggle('active');
+
+        let body = this.nextElementSibling; //document.getElementsByClassName('body');
+        console.log(this.nextElementSibling);
+        
+        if (body.style.display === 'block') {
+        body.style.display = 'none';
+    }
+        else body.style.display = 'block';
+
+
     })
 }
