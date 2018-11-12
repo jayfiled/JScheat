982245a3d98cf2eda1635a4d1515f264eaccabfb
Jayfiled <jayfiled@gmail.com>
2018-11-12 23:45:41 +1100

    make responsive

--- js/index.js
+++ js/index.js
@@ -4,11 +4,15 @@ for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
         this.classList.toggle('active');
 
-        let body = this.nextElementSibling; //document.getElementsByClassName('body');
-        console.log(this.nextElementSibling);
+        let body = this.nextElementSibling;
+        console.dir(body);
         
         if (body.style.display === 'block') {
             body.style.display = 'none';
-        } else body.style.display = 'block';
+            body.style.maxHeight = '0px';
+        } else {
+            body.style.display = 'block';
+            body.style.maxHeight = 'none';
+        }
     })
 }
